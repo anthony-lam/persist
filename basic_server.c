c509e4181a0cb09d239207cd3a6a4f2db476c335
AnthonyLam <alam6@stuy.edu>
2018-12-19 22:04:33 -0500

    done

--- basic_server.c
+++ basic_server.c
@@ -1,22 +1,33 @@
 #include "pipe_networking.h"
+#include <signal.h>
 
 
-void process(char* str){
 
+static void sighandler(int n){
+	if(n == SIGINT){
+		remove("wkp");
+		printf("[server] exiting\n");
+		exit(0);
+	}
 }
 
-int main() {
 
-  int to_client;
-  int from_client;
-  from_client = server_handshake( &to_client );
-  while(1){
-  	char recieved[1000];
-  	read(from_client, recieved, 1000);
-    strtok(recieved, "\n");
-    printf("Recieved -%s-\n", recieved);
-    process(recieved);
-    write(to_client, recieved, 1000);
-    printf("Sent -%s-\n", recieved);
-    }
+int main() {
+	signal(SIGINT, sighandler);
+	int to_client;
+	int from_client;
+	char recieved[1000];
+	int i;
+	while(1){
+		from_client = server_handshake( &to_client );
+		while(read(from_client, recieved, 1000)){
+			strtok(recieved, "\n");
+			printf("[server] Recieved -%s-\n", recieved);
+			for(i = 0; i < strlen(recieved); i++){
+				recieved[i] += 10;
+			}
+			write(to_client, recieved, 1000);
+			printf("[server] Sent -%s-\n", recieved);
+		}
+	}
 }
--- client.c
+++ client.c
@@ -6,13 +6,13 @@ int main() {
 	int from_server;
 	from_server = client_handshake( &to_server );
 	while(1){
-		printf("Enter input:\n");
+		printf("[client] Enter input:\n");
 		char input[1000];
 		char recieved[1000];
 		fgets(input, 1000, stdin);
 		write(to_server,input,1000);
 		read(from_server, recieved, 1000);
-    strtok(recieved, "\n");
-		printf("recieved: %s\n", recieved);
+		strtok(recieved, "\n");
+		printf("[client] Recieved: %s\n", recieved);
 	}
 }
